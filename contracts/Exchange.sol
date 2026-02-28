ccdb43cf13047df0b986fda8bddd8d3c42c3b2ca
Julian Liu <jul14nl1u@gmail.com>
2026-02-28 05:01:51 -0500

    implement Exchange similar to ERC-20-with-permit lecture example

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,29 +1,102 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.32;
 
-import { Order } from "./Order.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract Exchange {
-
-    // Events
-    event OrderDeployed(
-        address indexed order,
-        address indexed seller,
-        address tokenA,
-        address tokenB,
-        uint amountA,
-        uint amountB,
-        uint lifetime
+contract Exchange is EIP712 {
+    using ECDSA for bytes32;
+    
+    bytes32 public constant ORDER_TYPEHASH = keccak256(
+        "Order(address seller,address tokenA,address tokenB,uint256 amountA,uint256 amountB,uint256 deadline,uint256 nonce)"
     );
 
-    function createOrder(address tokenA, address tokenB, uint amountA, uint amountB, uint lifetime, bool emitOnChain) public returns (address) {
-        // TBD: check via ERC20 allowance(seller, spender) that Exchance is allowed to 
-        Order order = new Order(lifetime);
-        order.setSeller(msg.sender);
-        order.initialize(tokenA, tokenB, amountA, amountB);
+    // Keep track of used orders
+    mapping(bytes32 => bool) public used;
+
+    // Event that allows sellers to publish orders on-chain
+    event OrderPosted(
+        address seller,
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB,
+        uint256 deadline,
+        uint256 nonce,
+        bytes32 signature
+    );
+
+	  constructor() EIP712("GurtEX", "1") {}
+
+    function createOrder(
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB,
+        uint256 deadline,
+        uint256 nonce,
+        bytes32 signature,
+        bool emitOnChain
+    ) public returns (bool) {
+        // TBD: verify order values?
         if (emitOnChain) {
-            emit OrderDeployed(address(order), msg.sender, tokenA, tokenB, amountA, amountB, lifetime);
+            emit OrderPosted(
+              msg.sender,
+              tokenA,
+              tokenB,
+              amountA,
+              amountB,
+              deadline,
+              nonce,
+              signature
+            );
         }
-        return address(order);
+        return true;
+    }
+
+    function fillOrder(
+        address seller,
+        address spender,
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB,
+        uint256 deadline,
+        uint256 nonce,
+        bytes32 signature,
+        uint256 offer
+    ) public returns (bool) {
+        // check if the order has expired
+        require(block.timestamp <= deadline, "Order has expired");
+        // create the hash assuming the seller allows the transaction caller to spend amount of token
+        bytes32 structHash = keccak256(
+            abi.encode(
+                ORDER_TYPEHASH,
+                seller,
+                spender,
+                tokenA,
+                tokenB,
+                amountA,
+                amountB,
+                deadline,
+                nonce
+            )
+        );
+        bytes32 hash = _hashTypedDataV4(structHash);
+        // check if the order has been fully used already
+        require(!used[hash], "Order has been filled already");
+        // extract the signer
+        address from = ECDSA.recover(hash, signature);
+        // check that the signer is the seller
+        if (from != seller) {
+            return false;
+        }
+        // mark the order as used if fully used? check ERC20 allowance?
+        used[hash] = true;
+        // token exchange (check that this contract can spend on behalf of msg.sender)
+        ERC20(tokenA).transferFrom(seller, msg.sender, offer/amountB * amountA); // need safer math
+        ERC20(tokenB).transferFrom(msg.sender, seller, offer);
+        return true;
     }
 }
