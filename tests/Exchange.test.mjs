83a2d10c640bf5cec4fe3baa9dfa158dc9426734
Eddy Chen <eddy.al.chen@gmail.com>
2026-03-02 19:07:43 -0500

    Overpay buy order test

--- tests/Exchange.test.mjs
+++ tests/Exchange.test.mjs
@@ -344,7 +344,41 @@ describe("Exchange", function () {
     });
 
     describe("Overpay Buy Order", function (){
-        // Make after finding out what happens if overpay (try to buy too much)
+        let receipt;
+        let receipt2;
+        let signature;
+        const time = BigInt(60);
+        const nonce = getNonce();
+        beforeAll(async () => {
+            // Mint tokens to seller1 and buyer1
+            await seller1.writeContract({ address: tokenAddresses[0], abi: tokenABIs[0], functionName: "mint", args: [seller1.account.address, parseEther("2")] });
+            await buyer1.writeContract({ address: tokenAddresses[1], abi: tokenABIs[1], functionName: "mint", args: [buyer1.account.address, parseEther("2")] });
+            signature = await signOrderWithEIP712(
+                tokenAddresses[0],
+                tokenAddresses[1],
+                parseEther("1"),
+                parseEther("1"),
+                currentTime + time,
+                nonce,
+                seller1,
+                contract.address,
+                foundry.id
+            );
+            const { address, abi } = contract;
+            const hash = await seller1.writeContract({ address, abi, functionName: "createOrder", args: [tokenAddresses[0], tokenAddresses[1], parseEther("1"), parseEther("1"), currentTime + time, nonce, signature, true] });
+            receipt = await client.waitForTransactionReceipt({ hash });
+            receipts.push({label: "Sell Order", receipt});
+
+            // Approve the exchange contract to transfer tokens on behalf of seller1
+            await seller1.writeContract({ address: tokenAddresses[0], abi: tokenABIs[0], functionName: "approve", args: [contract.address, parseEther("1")] });
+        });
+        it("Should not allow buying more than the order has", async function () {
+            // approve buyer1 spending
+            await buyer1.writeContract({ address: tokenAddresses[1], abi: tokenABIs[1], functionName: "approve", args: [contract.address, parseEther("1.2")] });
+            // Create buy order
+            const hash2 = await buyer1.writeContract({ address, abi, functionName: "fillOrder", args: [seller1.account.address, tokenAddresses[0], tokenAddresses[1], parseEther("1"), parseEther("1"), currentTime + time, nonce, signature, parseEther("1.2")] });
+            await expect(hash2).rejects.toThrow("Failed to transfer tokenA");
+        });
     });
 
     describe("Expired Buy Order", function (){
