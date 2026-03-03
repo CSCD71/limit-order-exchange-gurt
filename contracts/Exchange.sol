4c708a66eed080fef5c068031fc991f6321f578d
Julian Liu <jul14nl1u@gmail.com>
2026-03-02 20:06:47 -0500

    implement fillable order check

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -49,7 +49,12 @@ contract Exchange is EIP712 {
         require(amountA > 0, "Value amountA must be positive");
         require(amountB > 0, "Value amountB must be positive");
         require(deadline > block.timestamp, "Deadline must be in the future");
-        // Verify signature
+        // Check that the seller has approved GurtEX
+        require(
+            ERC20(tokenA).allowance(msg.sender, address(this)) >= amountA,
+            "Seller has not approved GurtEX"
+        );
+        // Compute order hash
         bytes32 hash = _hashTypedDataV4(keccak256(
             abi.encode(
                 ORDER_TYPEHASH,
@@ -62,6 +67,7 @@ contract Exchange is EIP712 {
                 nonce
             )
         ));
+        // Check that order has not been filled already
         require(!used[hash], "Cannot create an order that has already been filled");
         // Check that the signer is the msg.sender
         address signer = ECDSA.recover(hash, signature);
@@ -88,7 +94,7 @@ contract Exchange is EIP712 {
         activeOrders[msg.sender] = 0;
     }
 
-    function fillOrder(
+    function isFillable(
         address seller,
         address tokenA,
         address tokenB,
@@ -96,14 +102,16 @@ contract Exchange is EIP712 {
         uint256 amountB,
         uint256 deadline,
         uint256 nonce,
-        bytes memory signature,
-        uint256 offer
+        bytes memory signature
     ) public returns (bool) {
         // Check that the order is active
         require(activeOrders[seller] == deadline, "Order is inactive");
         // Check if the order has expired
         require(block.timestamp <= deadline, "Order has expired");
-        // Create the hash assuming the seller allows the transaction caller to spend amount of token
+        // Check that this contract can spend on behalf of the seller
+        uint256 approvedAmount = ERC20(tokenB).allowance(seller, address(this));
+        require(approvedAmount > 0, "Order can no longer be filled");
+        // Create the hash
         bytes32 hash = _hashTypedDataV4(keccak256(
             abi.encode(
                 ORDER_TYPEHASH,
@@ -119,24 +127,49 @@ contract Exchange is EIP712 {
         // Check if the order has been fully used already
         require(!used[hash], "Order has been filled already");
         // Extract the signer
-        address from = ECDSA.recover(hash, signature);
+        address signer = ECDSA.recover(hash, signature);
         // Check that the signer is the seller
-        if (from != seller) {
-            return false;
+        require(signer == seller, "Invalid signature");
+        return true;
+    }
+
+    function fillOrder(
+        address seller,
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB,
+        uint256 deadline,
+        uint256 nonce,
+        bytes memory signature,
+        uint256 offer
+    ) public returns (bool) {
+        // Check that the offer is valid
+        require(offer > 0, "Offer amount must be positive");
+        require(offer <= amountB, "Offer amount cannot exceed order specification");
+        // Check that the order is fillable
+        require(
+            isFillable(seller, tokenA, tokenB, amountA, amountB, deadline, nonce, signature),
+            "Order is not fillable"
+        );
+        // Check that this contract can spend on behalf of msg.sender
+        uint256 buyerApprovedAmount = ERC20(tokenB).allowance(msg.sender, address(this));
+        require(buyerApprovedAmount >= offer, "Buyer has not approved GurtEX");
+        // Fixed Point Math from https://rareskills.io/post/solidity-fixed-point#converting-an-integer-to-a-fixed-point-number
+        // Github: https://github.com/transmissions11/solmate/blob/main/src/utils/FixedPointMathLib.sol
+        uint256 amountToTransfer = FixedPointMathLib.mulDivDown(offer, amountA, amountB);
+        // Check that the seller has approved the amount to transfer
+        uint256 sellerApprovedAmount = ERC20(tokenA).allowance(seller, address(this));
+        require(sellerApprovedAmount >= amountToTransfer, "Offer is too high for the remaining number of token A");
+        // Mark the order as used if to be fully filled
+        if (amountToTransfer == amountA || amountToTransfer == sellerApprovedAmount) {
+            used[hash] = true;
         }
-        // Mark the order as used if fully used? check ERC20 allowance?
-        // used[hash] = true;
-        // Token exchange (check that this contract can spend on behalf of msg.sender)
-        require(ERC20(tokenB).allowance(msg.sender, address(this)) >= offer, "Buyer has not approved GurtEX");
+        // Perform token exchange
         // Note on ERC20:transferFrom... 
         // this forum thread recommends SafeERC20? 
         // thread: https://forum.openzeppelin.com/t/should-i-check-for-transfer-result-for-an-erc20/37018
         // SafeERC20: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol
-
-        // Fixed Point Math from https://rareskills.io/post/solidity-fixed-point#converting-an-integer-to-a-fixed-point-number
-        // Github: https://github.com/transmissions11/solmate/blob/main/src/utils/FixedPointMathLib.sol
-        uint256 amountToTransfer = FixedPointMathLib.mulDivDown(offer, amountA, amountB);
-
         bool success = ERC20(tokenA).transferFrom(from, msg.sender, amountToTransfer);
         require(success, "Failed to transfer tokenA");
         success = ERC20(tokenB).transferFrom(msg.sender, from, offer);
